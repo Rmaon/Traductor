9a7a77fea09ffc993cfa9c6a2ee0391651f69082
ASUS TUF <ASUS TUF@DESKTOP-GGM9NV5>
2023-02-07 12:12:41 +0100

    version funcional

---
+++ Traductor/src/traductor/Traductor.java
@@ -0,0 +1,101 @@
+package traductor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ramon Caminero Arroyo <ramoncamineroarroyo@gmail.com>
+ */
+public class Traductor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean salir = true;
+        int menu;
+        Scanner sc = new Scanner(System.in);
+        boolean admin;
+        //bienvenida, incluir metodo
+        
+        do {
+            
+            menu = sc.nextInt();
+            
+            switch (menu) {//seleccion de rol
+                case 1://administrador
+                    admin = true;
+                    do {
+                        
+                        menu = sc.nextInt();
+                        
+                        switch (menu) {
+                            case 1://añadir palabras
+                                
+                                break;
+                            
+                            case 2: //salir
+                                
+                                salir = false;
+                            
+                            default:
+                                System.out.println("Introduce una opcion válida!!");
+                        }
+                    } while (salir);
+                    salir = true;
+                    break;
+                
+                case 2://invitado
+                    admin = false;
+                    
+                    do {
+                        
+                        
+                        
+                        switch (menu) {
+                            case 1://mostrar listado de palabras
+                                do {
+                                    
+                                    menu=sc.nextInt();
+                                    
+                                    switch (menu) {
+                                        case 1://mostrar tabla completa
+                                            
+                                            break;
+                                        case 2://mostrar lista español
+                                            
+                                            break;
+                                        case 3://mostrar lista gallego
+
+                                            break;
+                                        case 4://mostrar lista catalán
+                                            break;
+                                        case 5: //salir
+                                        default:
+                                            System.out.println("Introduce una opcion válida!!");
+                                    }
+                                } while (salir);
+                                salir=true;
+                                break;
+                            case 2://traducir palabras
+                                
+                            case 3://salir
+                                
+                                break;
+                            default:
+                                System.out.println("Introduce una opcion válida!!");
+                        }
+                    } while (salir);
+                    
+                    break;
+                case 3: //cerrar programa
+                    salir = false;
+                    break;
+                default:
+                    System.out.println("Introduce una opcion válida!!");
+            }
+            
+        } while (salir);
+    }
+    
+}
